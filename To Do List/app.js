0451172cbcd6a25cdac923c23179ede035ffb282
Eliakim Maillot <eliakim.maillot7@gmail.com>
2023-06-09 14:24:26 +0200

    fin todo list

--- To Do List/app.js
+++ To Do List/app.js
@@ -3,6 +3,7 @@ let task = document.getElementById("task");
 let addBtn = document.getElementById("add-btn");
 let taskArray = [];
 let list = document.getElementById("toDoList");
+let deleteBtn = document.querySelector(".delete-btn");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,18 +11,31 @@ form.addEventListener("submit", (e) => {
   showTask();
 });
 
+
 const stockTask = () => {
   if (task.value.length > 2) {
     taskArray.push(task.value);
-    console.log(taskArray)
+    /* console.log(taskArray) */
   }
 };
 
-const showTask = () => {
-  for (let i = 0; i < taskArray.length; i++) {
-    list.innerHTML += `<li>${taskArray[i]}</li>`;
-    task.value = "";
-    taskArray = [];
-    console.log(list.innerHTML)
-  }
+const refresh = () => {
+  task.value = "";
+  list.innerHTML = ''; //reinitialise l'affichage
 };
+
+const showTask = () => {
+  refresh();
+  for (let i = 0; i < taskArray.length; i++) {
+    /* console.log(i) */
+    list.innerHTML += `<div id="list-container"><div>${taskArray[i]}</div><button class="delete-btn" onclick="deleteTask(${i})">supprimer</button></div>`;
+  };
+};
+
+const deleteTask = (index) => {
+  taskArray = taskArray.filter((task, key) => key != index);
+  /* console.log(taskArray); */
+  showTask();
+}
+
+
